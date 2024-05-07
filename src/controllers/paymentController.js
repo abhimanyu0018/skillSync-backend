eea16af4bff22820deeb0a8438eefca5aec5b8e9
Abhimanyu Kanaujia <iabhimanyu.kanaujia@gmail.com>
2024-05-07 10:11:15 +0530

    some variables changes for deployment.

--- src/controllers/paymentController.js
+++ src/controllers/paymentController.js
@@ -7,7 +7,7 @@ import { Course } from "../models/course.model.js";
 // VERY VERY PRIVATE SECRET KEY FETCHING
 const instance = new razorpay({
   key_id: process.env.KEY,
-  key_secret: process.env.SECRET,
+  key_secret: process.env.RAZORPAY_SECRET,
 });
 
 // HERE ORDER DETAIL AND AMOUNT LOGIC IMPLEMENTED
@@ -30,7 +30,7 @@ export const paymentVerification = async (req, res) => {
     const { razorpay_order_id, razorpay_payment_id, razorpay_signature } = req.body;
     const body = razorpay_order_id + "|" + razorpay_payment_id;
     const expectedSignature = crypto
-      .createHmac("sha256", process.env.SECRET)
+      .createHmac("sha256", process.env.RAZORPAY_SECRET)
       .update(body.toString())
       .digest("hex");
 
